851aeb85c869c68b402a09bf937accc4feabdcc6
Iain Dillingham <iain@dillingham.me.uk>
2021-06-21 12:24:10 +0100

    Fix call to get_measure_tables

--- deciles_chart/cli.py
+++ deciles_chart/cli.py
@@ -13,7 +13,7 @@ import deciles_chart
     "output_dir", type=click.Path(exists=True, file_okay=False, path_type=pathlib.Path)
 )
 def main(input_dir, output_dir):
-    for measures_table in deciles_chart.get_measures_tables(input_dir):
+    for measures_table in deciles_chart.get_measure_tables(input_dir):
         measures_table = deciles_chart.drop_zero_denominator_rows(measures_table)
         deciles_table = deciles_chart.get_deciles_table(measures_table)
         chart = deciles_chart.get_deciles_chart(deciles_table)
